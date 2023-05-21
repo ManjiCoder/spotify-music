4b7d65e9b46c306ba4d4817e740bda0ee238d2ba
ManjiCoder <manjicoder@gmail.com>
2023-05-21 16:24:03 +0530

    Video 42 - ✔ Conditional rendering of UI for music in react native - ✔

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,39 @@
-import React from 'react';
-import {SafeAreaView, StatusBar, StyleSheet, Text} from 'react-native';
+/* eslint-disable prettier/prettier */
+import React, {useEffect, useState} from 'react';
+import {
+  ActivityIndicator,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+} from 'react-native';
+import {setupPlayer, addTrack} from '../musicPlayerSevices';
 
 function App(): JSX.Element {
+  const [isPlayerReady, setIsPlayerReady] = useState(false);
+
+  const setup = async () => {
+    let isSetup = await setupPlayer();
+
+    if (isSetup) {
+      await addTrack();
+    }
+
+    setIsPlayerReady(isSetup);
+  };
+
+  useEffect(() => {
+    setup();
+  }, []);
+
+  if (!isPlayerReady) {
+    return (
+      <SafeAreaView>
+        <ActivityIndicator />
+      </SafeAreaView>
+    );
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar />
